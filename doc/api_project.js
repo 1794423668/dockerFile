83109dda46e3ca9cb6e925a14b28f78476fc8f4e
tianyu <tianyu@spacesystech.com>
2019-04-26 11:55:54 +0800

    update

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,15 +1,15 @@
 define({
-  "title": "laidian",
-  "url": "https://avosapps.com",
-  "name": "dockerfile",
-  "version": "1.0.0",
-  "description": "",
+  "title": "Custom apiDoc browser title",
+  "url": "http//localhost:3000",
+  "name": "example",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-26T03:27:39.447Z",
+    "time": "2019-04-26T03:55:16.575Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
