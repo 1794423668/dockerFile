6f74aa2b4ed841508a10a802062ee22ab4472506
tianyu <tianyu@spacesystech.com>
2019-04-26 12:09:04 +0800

    update

--- doc/api_project.js
+++ doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-26T04:00:33.448Z",
+    "time": "2019-04-26T04:08:57.863Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,32 @@
 var actions = {
   /**
    * @api {get} /index/sgqy 测试接口
+   * @apiVersion 0.1.0
    * @apiName sgqy
-   * @apiDescription 测试接口
    * @apiGroup leTv
-   * @apiSuccessExample {json} Response 200 Example:
-   *    HTTP/1.1 200 OK
-   *    {"code" : 0,
-   *     "msg" : ""成功
-   *   }
+   * @apiSuccess {String} firstname Firstname of the User.
+   * @apiSuccess {String} lastname  Lastname of the User.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "firstname": "John",
+   *       "lastname": "Doe"
+   *     }
+   *
+   * @apiError UserNotFound The id of the User was not found.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {
+   *       "error": "UserNotFound"
+   *     }
    */
   sgqy: function (req, res) {
     res.send({ success: 1, data: "大哥啊真是好大哥真是个大傻吊啊! " })
   },
   api: function (req, res) {
-    res.sendFile ('f:/github/dockerFile/doc/index.html')
+    res.sendFile('f:/github/dockerFile/doc/index.html')
   },
 };
 module.exports = actions;
